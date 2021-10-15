f7aa85e07400a88aadedf606a0983c670aeed043
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-10-15 08:10:34 +0200

    enable plasma

--- hosts/acer/xserver.nix
+++ hosts/acer/xserver.nix
@@ -3,22 +3,10 @@
 {
   services.xserver = {
     enable = true;
-    videoDrivers = [ "amdgpu" ];
-    desktopManager = { xterm.enable = false; };
+    desktopManager = { plasma5.enable = false; };
     displayManager = {
       sddm.enable = true;
-      defaultSession = "none+qtile";
     };
-    windowManager.qtile.enable = true;
     layout = "pl";
-    xkbOptions = "caps:swapescape";
-    libinput = {
-      enable = true;
-      touchpad = {
-        naturalScrolling = true;
-        disableWhileTyping = true;
-      };
-      mouse = { accelProfile = "flat"; };
-    };
   };
 }
