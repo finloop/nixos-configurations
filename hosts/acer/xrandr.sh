b62941019f5027540e28b9564390b5e496ba7d8d
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-11-07 16:53:59 +0100

    Update xrandr script

--- hosts/acer/xrandr.sh
+++ hosts/acer/xrandr.sh
@@ -1,3 +1,6 @@
 xrandr --setprovideroutputsource 1 0
 xrandr --screen 0 --fb 3840x1080
 xrandr --output HDMI-1-0 --mode 1920x1080 -r 59.94 --pos 1920x0
+pkill gammastep
+gammastep-indicator &
+disown
