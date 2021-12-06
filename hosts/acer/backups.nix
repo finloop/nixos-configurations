121c95d484b5966fdedbc9daa352daa7ff751046
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-12-06 20:47:03 +0100

    Exclude files from backup

--- hosts/acer/backups.nix
+++ hosts/acer/backups.nix
@@ -7,13 +7,35 @@
   };
   services.restic.backups = {
     b2 = {
-      paths = [ "/home/pk/Projects" ];
+      paths = [
+        "/home/pk"
+      ];
       repository = "rclone:b2:pk-backup-home";
       initialize = true;
       passwordFile = config.age.secrets.restic.path;
       timerConfig = {
         OnCalendar = "00:05";
       };
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+        "--keep-yearly 12"
+      ];
+
+      extraBackupArgs = [
+        "--exclude-caches"
+        "--exclude=/home/pk/.conda"
+        "--exclude=/home/pk/.npm"
+        "--exclude=/home/pk/.local"
+        "--exclude=/home/pk/bin"
+        "--exclude=/home/pk/Desktop"
+        "--exclude=/home/pk/Downloads"
+        "--exclude=/home/pk/miniconda3"
+        "--exclude=/home/pk/Videos"
+        "--exclude=/home/pk/VirtualBox VM"
+        "--exclude=/home/pk/.jupyter"
+      ];
 
       rcloneConfigFile = config.age.secrets.rclone.path;
     };
